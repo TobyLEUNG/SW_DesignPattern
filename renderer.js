9a04c63fdf62cdfbabb01dde5bfffb90d5f8fecb
TobyLEUNG <todogy@gmail.com>
2024-08-06 13:40:46 -0400

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -61,28 +61,3 @@ export class Renderer {
     }
 }
 
-// inputHandler.js
-export class InputHandler {
-    constructor() {
-        this.keyDownHandlers = [];
-        this.keyUpHandlers = [];
-        window.addEventListener('keydown', this.handleKeyDown.bind(this));
-        window.addEventListener('keyup', this.handleKeyUp.bind(this));
-    }
-
-    onKeyDown(handler) {
-        this.keyDownHandlers.push(handler);
-    }
-
-    onKeyUp(handler) {
-        this.keyUpHandlers.push(handler);
-    }
-
-    handleKeyDown(event) {
-        this.keyDownHandlers.forEach(handler => handler(event.key));
-    }
-
-    handleKeyUp(event) {
-        this.keyUpHandlers.forEach(handler => handler(event.key));
-    }
-}
